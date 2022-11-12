8828a95272ff229afe9a2e3ccd5be2f5021412b8
Floyd Hightower <floyd.hightower27@gmail.com>
2022-11-12 16:37:07 -0500

    Simplify solution

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,10 +1,7 @@
 STRING_FOR_ME = 'one for me.'
 
 class TwoFer
-  def self.two_fer(name = nil)
-    if (name == nil)
-      name = "you"
-    end
+  def self.two_fer(name = "you")
     "One for #{name}, #{STRING_FOR_ME}"
   end
 end
