21060d0372ae6b07b2197fd4ac0557e9cfa7c83d
Floyd Hightower <floyd.hightower27@gmail.com>
2022-11-14 11:28:19 -0500

    Minor improvements

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -3,13 +3,8 @@ class SimpleCalculator
   UnsupportedOperation = Class.new(StandardError)
 
   def self.calculate(first_operand, second_operand, operation)
-    if !ALLOWED_OPERATIONS.include? operation
-      raise UnsupportedOperation.new("The given operation (#{operation}) is invalid. Please use a valid operation: #{ALLOWED_OPERATIONS}.")
-    end
-
-    if !first_operand.is_a?(Numeric) or !second_operand.is_a?(Numeric)
-      raise ArgumentError.new("The first and second operands must both be numbers.")
-    end
+    raise UnsupportedOperation.new("The given operation (#{operation}) is invalid. Please use a valid operation: #{ALLOWED_OPERATIONS}.") unless ALLOWED_OPERATIONS.include?(operation)
+    raise ArgumentError.new("The first and second operands must both be numbers.") unless first_operand.is_a?(Numeric) and second_operand.is_a?(Numeric)
 
     result = first_operand.public_send(operation, second_operand)
     "#{first_operand} #{operation} #{second_operand} = #{result}"
