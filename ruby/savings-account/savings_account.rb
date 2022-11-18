214dd389c81225b28040846c2cb8e5c1b36e953f
Floyd Hightower <floyd.hightower27@gmail.com>
2022-11-17 19:24:10 -0500

    Add exercism

---
+++ ruby/savings-account/savings_account.rb
@@ -0,0 +1,35 @@
+module SavingsAccount
+  INTEREST_RATE_MAXIMUMS = {0=> 3.213, 1000=> 0.5, 5000=> 1.621, Float::INFINITY=> 2.475}
+
+  def self.interest_rate(balance)
+    INTEREST_RATE_MAXIMUMS.find { |k, v| balance < k}[1]
+  end
+
+  def self.annual_balance_update(balance)
+    # This is a hack for calculating the interest and adding it to the balance...
+    # Doing this: balance + interest_rate * balance
+    # Is the same as doing: (1 + interest_rate) * balance
+    (1 + (self.interest_rate(balance) / 100)) * balance
+  end
+
+  def self.years_before_desired_balance(current_balance, desired_balance)
+    balance = current_balance
+    year = 0
+    while balance < desired_balance
+      year += 1
+      balance = self.annual_balance_update(balance)
+    end
+
+=begin
+Alternatively:
+
+```
+until balance > desired_balance:
+  year += 1
+  balance = self.annual_balance_update(balance)
+end
+```
+=end
+    year
+  end
+end
