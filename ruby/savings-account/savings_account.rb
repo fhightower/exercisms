c3eadf797cf0cbe1cba19f72756886e2b1c4a2fd
Floyd Hightower <floyd.hightower27@gmail.com>
2022-11-17 19:25:51 -0500

    Minor improvements

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -7,8 +7,8 @@ module SavingsAccount
 
   def self.annual_balance_update(balance)
     # This is a hack for calculating the interest and adding it to the balance...
-    # Doing this: balance + interest_rate * balance
-    # Is the same as doing: (1 + interest_rate) * balance
+    # Doing this: (1 + interest_rate) * balance
+    # Is the same as doing: balance + (interest_rate * balance)
     (1 + (self.interest_rate(balance) / 100)) * balance
   end
 
