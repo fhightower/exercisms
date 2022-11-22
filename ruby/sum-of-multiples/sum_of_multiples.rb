13967533cfd13c5a31d709c51e63888ad19ae119
Floyd Hightower <floyd.hightower27@gmail.com>
2022-11-22 09:55:25 -0500

    Make func more efficient

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -2,7 +2,7 @@ class SumOfMultiples
   attr_reader :integers
 
   def to(max_integer)
-    integers.map { |i| multiples_up_to(multiples_of: i, max_integer: max_integer - 1) }.flatten.uniq.sum
+    1.upto(max_integer - 1).select { |counter| integers.any? { |i| multiple?(numerator: counter, denominator: i) } }.sum
   end
 
   private
@@ -11,11 +11,8 @@ class SumOfMultiples
     @integers = integers
   end
 
-  def multiples_up_to(multiples_of:, max_integer:)
-    1.upto(max_integer).select { |n| multiple?(numerator: n, denominator: multiples_of) }
-  end
-
   def multiple?(numerator:, denominator:)
     (numerator % denominator).zero?
   end
 end
+
