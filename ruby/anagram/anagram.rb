45a65db1008dbb148dd0474b9caff47e23f3dbcf
Floyd Hightower <floyd.hightower27@gmail.com>
2022-12-02 11:30:14 -0500

    Improve solution

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,24 +1,28 @@
-class String
-  def anagram_of?(possible_anagram)
-    return false unless self.length == possible_anagram.length
-    return false unless self != possible_anagram
-    self.chars.sort == possible_anagram.chars.sort
-  end
+module ExtendedString
+  refine String do
+    def anagram_of?(possible_anagram)
+      return false unless self.length == possible_anagram.length
 
-  def clean
-    return self.downcase
+      downcased_possible_anagram = possible_anagram.downcase
+      downcased_self = self.downcase
+
+      return false unless downcased_self != downcased_possible_anagram
+      downcased_self.chars.sort == downcased_possible_anagram.chars.sort
+    end
   end
 end
+
+using ExtendedString
 
 class Anagram
   attr_reader :target
 
   def initialize(target)
-    @target = target.clean
+    @target = target
   end
 
   def match(possible_anagrams)
-    possible_anagrams.select { |word| word.clean.anagram_of?(target) }
+    possible_anagrams.select { |word| word.anagram_of?(target) }
   end
 end
 
