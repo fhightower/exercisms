30e7d99221f6a84b2acaf4a157398eb69a9587c9
Floyd Hightower <floyd.hightower27@gmail.com>
2022-12-12 12:31:02 -0500

    Change class to module

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,4 +1,4 @@
-class ETL
+module ETL
   def self.transform(old_data)
     old_data.each_with_object({}) { |(score, letters), new_data| letters.each { |letter| new_data[letter.downcase] = score } }
   end
