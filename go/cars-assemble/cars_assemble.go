482e81a21848646cd8fd55165a57834a09eeca10
Floyd Hightower <floyd.hightower27@gmail.com>
2023-08-14 10:39:41 -0400

    Add new exercism

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,25 @@
+package cars
+
+const costPerCar int = 10000
+const costPerTenCars int = 95000
+
+func convertPercentToDecimal(percent float64) float64 {
+	return percent / 100
+}
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * convertPercentToDecimal(successRate)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate)) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	return uint(((carsCount / 10) * costPerTenCars) + ((carsCount % 10) * costPerCar))
+}
